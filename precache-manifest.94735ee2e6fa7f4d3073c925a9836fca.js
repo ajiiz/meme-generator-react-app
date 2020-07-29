e40f1ae92f59b7dfacfed34990702588ff9e5c9b
ajiiz <opticpiter@gmail.com>
2020-07-29 19:18:42 +0200

    Updates

---
+++ precache-manifest.94735ee2e6fa7f4d3073c925a9836fca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e7e95861b64cedaf43df457a2da73f",
+    "url": "/meme-generator-react-app/index.html"
+  },
+  {
+    "revision": "e16a8ef929feef1626fa",
+    "url": "/meme-generator-react-app/static/css/main.3ee0786a.chunk.css"
+  },
+  {
+    "revision": "b4a1b86a11a76c99cdf1",
+    "url": "/meme-generator-react-app/static/js/2.246177ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator-react-app/static/js/2.246177ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e16a8ef929feef1626fa",
+    "url": "/meme-generator-react-app/static/js/main.2e0ddecc.chunk.js"
+  },
+  {
+    "revision": "0ef44bfe11256a717be7",
+    "url": "/meme-generator-react-app/static/js/runtime-main.c3974f8e.js"
+  }
+]);
