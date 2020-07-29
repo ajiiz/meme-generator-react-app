fd5f1758b4b2e882d62b4927beeaba125f0c8741
ajiiz <opticpiter@gmail.com>
2020-07-29 15:08:24 +0200

    Added fetching data method

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -4,14 +4,27 @@ class MemeGenerator extends Component {
     constructor() {
         super()
         this.state ={
-            topText :"",
-            bottomText :"",
-            randomImage :"http://i.imgflip.com/1bij.jpg"
+            topText: "",
+            bottomText: "",
+            randomImage: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                const {memes} = response.data
+                this.setState({
+                    allMemeImgs : memes
+                })
+            })
     }
     render(){
         return (
-            <h1>This is meme generator!</h1>
+            <div>
+            </div>
         )
     }
 }
